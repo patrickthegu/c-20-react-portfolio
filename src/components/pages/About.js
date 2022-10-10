6fa1b56c32fea32614e424c2d0b7dc456219509e
Patrick Gu <patrickthegu@gmail.com>
2022-10-10 19:47:24 +1100

    initial commit, still need to style and fill content

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Hi my name is Patrick Gu,  I am currently taking a full stack development bootcamp having previously
+        studied mechanical engineering and working in commercial construction.  
+
+        I am based in Melbourne, Australia
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Email
+        Github
+        LinkedIn
+        Resume
+      </p>
+    </div>
+  );
+}
